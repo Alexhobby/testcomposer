599e7fa3d40fdd4b9777a7ff656126f673a7b04b
Alexandr <i9312452497@gmail.com>
2021-09-25 20:16:11 +0300

    ff

---
+++ Test/Test.php
@@ -0,0 +1,16 @@
+<?php
+
+//namespace Testcomposer\Test;
+
+class Test
+{
+
+    public function __construct()
+    {
+    }
+
+    public function answ()
+    {
+        echo "test ok";
+    }
+}
---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+require_once '../testcomposer/Test/Test.php';
+
+//use Testcomposer\Test as Test;
+
+
+
+
+
+$t = new Test();
+$t->answ();
